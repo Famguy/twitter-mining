fd61ca6c33998c75464ed9c2ef1ef6dc8939ccea
Sagun Pai <sagung.pai@gmail.com>
2016-01-20 01:59:04 +0530

    auth stuff

---
+++ oauth.R
@@ -0,0 +1,9 @@
+library(twitteR)
+
+consumer.key <- "NmmwYFIkJ8ePcZSyiCiOQ"
+consumer.secret <- "KOdDBCvuZZfywqDbaPIpE8HQykEuqJ4PjM0WaUOUUUU"
+
+access.token.key <- "1654961214-BVFMMFyMt9vvexPwZ8I8YQW9sqbu5vf8ML8OQFF"
+access.token.secret <- "u1sdJ3bseOpPggtaY4yYKpGIS9eIThwg9106fuEO0E4"
+
+setup_twitter_oauth(consumer.key, consumer.secret, access.token.key, access.token.secret)
